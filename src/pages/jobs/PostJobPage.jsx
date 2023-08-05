69820ae52e21557c0df113ef76f608a855e34f21
Pasang Lama <pasangtheeng14@gmail.com>
2023-08-05 14:24:02 +0545

    Last commit

--- src/pages/jobs/PostJobPage.jsx
+++ src/pages/jobs/PostJobPage.jsx
@@ -5,6 +5,7 @@ import "../Form.css";
 
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
+import TableLoader from "../../components/loader components/SpinnerLoader";
 
 function PostJobPage() {
   const { id } = useParams();
@@ -24,6 +25,7 @@ function PostJobPage() {
     description: "",
     images: null,
   });
+  const [loading, setLoading] = useState(false);
 
   const [fileName, setFileName] = useState("No selected file");
 
@@ -42,16 +44,76 @@ function PostJobPage() {
 
   useEffect(() => {
     if (id) {
-      axios.get(`${import.meta.env.VITE_SERVER_URL}/job/${id}`).then((res) => {
-        console.log(res);
-        setJob(res.data);
+      setLoading(true);
+      axios
+        .get(`${import.meta.env.VITE_SERVER_URL}/job/${id}`)
+        .then(
+          ({
+            data: {
+              title,
+              company,
+              location,
+              phone,
+              website,
+              requirements,
+              salary,
+              vacancy,
+              category,
+              posted_date,
+              closing_date,
+              description,
+              images,
+            },
+          }) => {
+            setJob({
+              title,
+              company,
+              location,
+              phone,
+              website,
+              requirements,
+              salary,
+              vacancy,
+              category,
+              posted_date: posted_date.slice(0, 10),
+              closing_date: closing_date.slice(0, 10),
+              description,
+              images: null,
+            });
+            urlToFile(images);
+          }
+        )
+        .finally(() => {
+          setLoading(false);
+        });
+    } else {
+      setJob({
+        title: "",
+        company: "",
+        location: "",
+        phone: "",
+        website: "",
+        requirements: "",
+        salary: "",
+        vacancy: "",
+        category: [],
+        posted_date: "",
+        closing_date: "",
+        description: "",
+        images: null,
       });
     }
-  }, []);
+  }, [id]);
 
-  useEffect(() => {
-    console.log(job);
-  }, [job]);
+  const urlToFile = (name) => {
+    fetch(`https://apiforjob.onrender.com/${name}`)
+      .then((response) => response.blob())
+      .then((blob) => {
+        const file = new File([blob], name, { type: blob.type });
+        setJob((prev) => ({ ...prev, images: file }));
+        setFileName(name);
+      });
+  };
 
   const handleCategoryChange = (event) => {
     const cat = event.target.value;
@@ -113,6 +175,11 @@ function PostJobPage() {
       url = `${import.meta.env.VITE_SERVER_URL}/job/${id}`;
     }
 
+    // for (let pair of form_data.entries()) {
+    //   if (pair[0] === "images") console.log(pair[0] + ", " + pair[1].name);
+    //   else console.log(pair[0] + ", " + pair[1]);
+    // }
+
     axios[method](url, form_data, {
       headers: {
         Authorization: `Bearer ${localStorage.getItem("token")}`,
@@ -136,6 +203,7 @@ function PostJobPage() {
           type="checkbox"
           name={category.name}
           value={category.value}
+          checked={job.category.includes(category.value)}
         />
         <label htmlFor={category.name}>{category.name}</label>
       </div>
@@ -144,212 +212,213 @@ function PostJobPage() {
 
   return (
     <div className="page-wrapper">
-      <h2 className="form-title">Create a Job</h2>
-      <form
-        onSubmit={handleSubmit}
-        className="form-wrapper"
-        encType="multipart/form-data"
-      >
-        <div className="input-section">
-          <div className="form-field">
-            <label htmlFor="company">Company Name</label>
-            <input
-              type="text"
-              onChange={handleChange}
-              name="company"
-              value={job.company}
-              required
-              placeholder="Name *"
-            />
-          </div>
-          <div className="form-field">
-            <label htmlFor="website">Company Website</label>
-            <input
-              type="text"
-              onChange={handleChange}
-              value={job.website}
-              name="website"
-              required
-              placeholder="Website Link *"
-            />
-          </div>
-        </div>
-        <div className="input-section">
-          <div className="form-field">
-            <label htmlFor="title">Job Title</label>
-            <input
-              type="text"
-              onChange={handleChange}
-              value={job.title}
-              name="title"
-              required
-              placeholder="Title *"
-            />
-          </div>
-        </div>
-        <div className="input-section">
-          <div className="form-field">
-            <label htmlFor="location">Job Location</label>
-            <input
-              type="text"
-              onChange={handleChange}
-              value={job.location}
-              name="location"
-              required
-              placeholder="Location *"
-            />
-          </div>
-          <div className="form-field">
-            <label htmlFor="salary">Salary</label>
-            <input
-              type="number"
-              onChange={handleChange}
-              value={job.salary}
-              name="salary"
-              required
-              placeholder="Salary *"
-            />
-          </div>
-        </div>
-        <div className="input-section">
-          <div className="form-field">
-            <label htmlFor="phone">Contact</label>
-            <input
-              type="number"
-              onChange={handleChange}
-              value={job.phone}
-              name="phone"
-              required
-              placeholder="Contact *"
-            />
-          </div>
-          <div className="form-field">
-            <label htmlFor="vacancy">Vacancy No.</label>
-            <input
-              type="number"
-              onChange={handleChange}
-              value={job.vacancy}
-              name="vacancy"
-              required
-              placeholder="Vacancy No. *"
-            />
-          </div>
-        </div>
-        <div className="input-section">
-          <div className="form-field">
-            <label htmlFor="posted_date">Application Start</label>
-            <input
-              type="date"
-              onChange={handleChange}
-              value={job.posted_date}
-              name="posted_date"
-              required
-            />
-          </div>
-          <div className="form-field">
-            <label htmlFor="closing_date">Application Deadline</label>
-            <input
-              type="date"
-              onChange={handleChange}
-              value={job.closing_date}
-              name="closing_date"
-              required
-            />
-          </div>
-        </div>
-        <div className="input-section">
-          <div className="form-field">
-            <label htmlFor="requirements">Job Requirement</label>
-            <textarea
-              value={job.requirements}
-              onChange={handleChange}
-              required
-              name="requirements"
-              placeholder="Job Requirement"
-            />
-          </div>
-          <div className="form-field">
-            <label htmlFor="description">Job Description</label>
-            <textarea
-              value={job.description}
-              onChange={handleChange}
-              required
-              name="description"
-              placeholder="Job Description"
-            />
-          </div>
-        </div>
-        <div className="input-section">
-          <div id="category-heading" className="input-category-wrapper">
-            <span>Job Category</span>
-            <p>#Select at least one category!</p>
-            <div className="input-category-container">
-              <div className="category-input-section">
-                {renderCategories.slice(0, 5)}
-              </div>
-              <div className="category-input-section">
-                {renderCategories.slice(5)}
-              </div>
+      <h2 className="form-title">{id ? "Edit Job" : "Create a Job"}</h2>
+      {loading ? (
+        <TableLoader />
+      ) : (
+        <form
+          onSubmit={handleSubmit}
+          className="form-wrapper"
+          encType="multipart/form-data"
+        >
+          <div className="input-section">
+            <div className="form-field">
+              <label htmlFor="company">Company Name</label>
+              <input
+                type="text"
+                onChange={handleChange}
+                name="company"
+                value={job.company}
+                required
+                placeholder="Name *"
+              />
+            </div>
+            <div className="form-field">
+              <label htmlFor="website">Company Website</label>
+              <input
+                type="text"
+                onChange={handleChange}
+                value={job.website}
+                name="website"
+                required
+                placeholder="Website Link *"
+              />
             </div>
           </div>
-
-          <div className="form-field">
-            <label>Upload Image</label>
-            <div
-              onClick={() => {
-                document.querySelector(".image-input").click();
-              }}
-              className="image-input-wrapper"
-            >
+          <div className="input-section">
+            <div className="form-field">
+              <label htmlFor="title">Job Title</label>
               <input
-                onChange={({ target: { files } }) => {
-                  console.log(files[0]);
-                  files && setFileName(files[0].name);
-                  if (files) {
-                    setJob({ ...job, images: files[0] });
-                  }
-                }}
-                className="image-input"
-                name="images"
-                filename="images"
-                type="file"
-                // formEncType="multipart/form-data"
-                accept="image/*"
-                hidden
+                type="text"
+                onChange={handleChange}
+                value={job.title}
+                name="title"
+                required
+                placeholder="Title *"
               />
+            </div>
+          </div>
+          <div className="input-section">
+            <div className="form-field">
+              <label htmlFor="location">Job Location</label>
+              <input
+                type="text"
+                onChange={handleChange}
+                value={job.location}
+                name="location"
+                required
+                placeholder="Location *"
+              />
+            </div>
+            <div className="form-field">
+              <label htmlFor="salary">Salary</label>
+              <input
+                type="number"
+                onChange={handleChange}
+                value={job.salary}
+                name="salary"
+                required
+                placeholder="Salary *"
+              />
+            </div>
+          </div>
+          <div className="input-section">
+            <div className="form-field">
+              <label htmlFor="phone">Contact</label>
+              <input
+                type="number"
+                onChange={handleChange}
+                value={job.phone}
+                name="phone"
+                required
+                placeholder="Contact *"
+              />
+            </div>
+            <div className="form-field">
+              <label htmlFor="vacancy">Vacancy No.</label>
+              <input
+                type="number"
+                onChange={handleChange}
+                value={job.vacancy}
+                name="vacancy"
+                required
+                placeholder="Vacancy No. *"
+              />
+            </div>
+          </div>
+          <div className="input-section">
+            <div className="form-field">
+              <label htmlFor="posted_date">Application Start</label>
+              <input
+                type="date"
+                onChange={handleChange}
+                value={job.posted_date}
+                name="posted_date"
+                required
+              />
+            </div>
+            <div className="form-field">
+              <label htmlFor="closing_date">Application Deadline</label>
+              <input
+                type="date"
+                onChange={handleChange}
+                value={job.closing_date}
+                name="closing_date"
+                required
+              />
+            </div>
+          </div>
+          <div className="input-section">
+            <div className="form-field">
+              <label htmlFor="requirements">Job Requirement</label>
+              <textarea
+                value={job.requirements}
+                onChange={handleChange}
+                required
+                name="requirements"
+                placeholder="Job Requirement"
+              />
+            </div>
+            <div className="form-field">
+              <label htmlFor="description">Job Description</label>
+              <textarea
+                value={job.description}
+                onChange={handleChange}
+                required
+                name="description"
+                placeholder="Job Description"
+              />
+            </div>
+          </div>
+          <div className="input-section">
+            <div id="category-heading" className="input-category-wrapper">
+              <span>Job Category</span>
+              <p>#Select at least one category!</p>
+              <div className="input-category-container">
+                <div className="category-input-section">
+                  {renderCategories.slice(0, 5)}
+                </div>
+                <div className="category-input-section">
+                  {renderCategories.slice(5)}
+                </div>
+              </div>
+            </div>
 
-              {!id &&
-                (job.images ? (
+            <div className="form-field">
+              <label>Upload Image</label>
+              <div
+                onClick={() => {
+                  document.querySelector(".image-input").click();
+                }}
+                className="image-input-wrapper"
+              >
+                <input
+                  onChange={({ target: { files } }) => {
+                    files && setFileName(files[0].name);
+                    if (files) {
+                      setJob({ ...job, images: files[0] });
+                    }
+                  }}
+                  className="image-input"
+                  name="images"
+                  filename="images"
+                  type="file"
+                  // formEncType="multipart/form-data"
+                  accept="image/*"
+                  hidden
+                />
+                {job.images ? (
                   <img src={URL.createObjectURL(job.images)} />
                 ) : (
                   <>
                     <MdCloudUpload size={60} color="#e3e3e3" />
                     <p>Browse Files to upload</p>
                   </>
-                ))}
-            </div>
+                )}
+              </div>
 
-            <div className="image-remove-btn-wrapper">
-              <AiFillFileImage color="#338573" />
-              <div>
-                <span>{fileName}</span>
-                <button
-                  type="button"
-                  onClick={() => {
-                    setFileName("No selected file");
-                    setJob({ ...job, images: null });
-                  }}
-                >
-                  Remove
-                </button>
+              <div className="image-remove-btn-wrapper">
+                <AiFillFileImage color="#338573" />
+                <div>
+                  <span>{fileName}</span>
+                  <button
+                    type="button"
+                    onClick={() => {
+                      setFileName("No selected file");
+                      setJob({ ...job, images: null });
+                    }}
+                  >
+                    Remove
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <button type="submit" className="form-btn post-btn">
-          Post Job
-        </button>
-      </form>
+          <button type="submit" className="form-btn post-btn">
+            Post Job
+          </button>
+        </form>
+      )}
     </div>
   );
 }
