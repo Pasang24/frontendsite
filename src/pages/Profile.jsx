bff5efac7cfc091a4654c5220623b985cf0518f8
Pasang Lama <pasangtheeng14@gmail.com>
2023-08-05 15:32:04 +0545

    Added few more loaders

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./jobs/JobsTable.css"
+import SpinnerLoader from "../components/loader components/SpinnerLoader";
+import "./jobs/JobsTable.css";
 
 function Profile() {
   const [user, setUser] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
+    setLoading(true);
     axios
       .get(`https://apiforjob.onrender.com/api/getuserdata`, {
         headers: {
@@ -14,35 +18,39 @@ function Profile() {
       .then((res) => {
         console.log(res.data);
         setUser(res.data);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   }, []);
   return (
     <div className="page-wrapper">
-        <div className="job-table-heading">
-          <h2>Profile</h2>
-        </div>
+      <h2 className="job-table-heading">Profile</h2>
 
-      <div className="list">
-        <h2>
-          Name : <span>{user.name}</span>
-        </h2>
-        <h2>
-          Email : <span>{user.email}</span>
-        </h2>
-        <h2>
-          PhoneNumber : <span>{user.phoneNumber}</span>
-        </h2>
-        {
-        (localStorage.getItem("role") === "applicant") ? (
+      {loading ? (
+        <SpinnerLoader />
+      ) : (
+        <div className="list">
           <h2>
-            Education : <span>{user.education}</span>
+            Name : <span>{user.name}</span>
           </h2>
-        ) : (
           <h2>
-            Company : <span>{user.company}</span>
+            Email : <span>{user.email}</span>
           </h2>
-        )}
-      </div>
+          <h2>
+            PhoneNumber : <span>{user.phoneNumber}</span>
+          </h2>
+          {localStorage.getItem("role") === "applicant" ? (
+            <h2>
+              Education : <span>{user.education}</span>
+            </h2>
+          ) : (
+            <h2>
+              Company : <span>{user.company}</span>
+            </h2>
+          )}
+        </div>
+      )}
     </div>
   );
 }
--- src/pages/jobs/ShowJob.jsx
+++ src/pages/jobs/ShowJob.jsx
@@ -2,81 +2,112 @@ import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./JobsTable.css";
+import SpinnerLoader from "../../components/loader components/SpinnerLoader";
+import { createPortal } from "react-dom";
+import LoaderContainer from "../../components/loader components/LoaderContainer";
 
 function ShowJob() {
   const navigate = useNavigate();
   const { id } = useParams();
   const [job, setJob] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [applying, setApplying] = useState(false);
+
   useEffect(() => {
-    axios.get(`${import.meta.env.VITE_SERVER_URL}/job/${id}`).then((res) => {
-      console.log(res);
-      setJob(res.data);
-    });
+    setLoading(true);
+    axios
+      .get(`${import.meta.env.VITE_SERVER_URL}/job/${id}`)
+      .then((res) => {
+        console.log(res);
+        setJob(res.data);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   }, []);
 
   const applyJob = (job) => {
-    axios.post(`${import.meta.env.VITE_SERVER_URL}/applyjob`, {
-      jobs:{job_id:job._id}
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`
-      }
-    })
-    .then(res => {
-      console.log(res);
-      navigate("/appliedjobs")
-    })
-  }
+    setApplying(true);
+    axios
+      .post(
+        `${import.meta.env.VITE_SERVER_URL}/applyjob`,
+        {
+          jobs: { job_id: job._id },
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+        navigate("/appliedjobs");
+      })
+      .finally(() => {
+        setApplying(false);
+      });
+  };
 
   return (
-    <div className="page-wrapper">
-      <div className="job-table-heading">
-        <span>
-          {job.title} - {job.company}
-        </span>
-      </div>
+    <>
+      <div className="page-wrapper">
+        {loading ? (
+          <SpinnerLoader />
+        ) : (
+          <>
+            <div className="job-table-heading">
+              <span>
+                {job.title} - {job.company}
+              </span>
+            </div>
 
-      <div className="showcontainer">
-        {
-          (localStorage.getItem("role") === "applicant")
-          &&
-          <div className="btn">
-            <button onClick={() => applyJob(job)}>Apply This job</button>
-          </div>
-        }
-        <div className="list">
-          <h2>
-            Title : <span>{job.title}</span>{" "}
-          </h2>
-          <h2>
-            Location : <span>{job.location}</span>{" "}
-          </h2>
-          <h2>
-            Company : <span>{job.company}</span>{" "}
-          </h2>
-          <h2>
-            Phone : <span>{job.phone}</span>{" "}
-          </h2>
-          <h2>
-            Website : <span>{job.website}</span>{" "}
-          </h2>
-          <h2>
-            Salary : <span>{job.salary}</span>{" "}
-          </h2>
-          <h2>
-            Vacancy : <span>{job.vacancy}</span>{" "}
-          </h2>
-        </div>
+            <div className="showcontainer">
+              {localStorage.getItem("role") === "applicant" && (
+                <div className="btn">
+                  <button onClick={() => applyJob(job)}>Apply This job</button>
+                </div>
+              )}
+              <div className="list">
+                <h2>
+                  Title : <span>{job.title}</span>{" "}
+                </h2>
+                <h2>
+                  Location : <span>{job.location}</span>{" "}
+                </h2>
+                <h2>
+                  Company : <span>{job.company}</span>{" "}
+                </h2>
+                <h2>
+                  Phone : <span>{job.phone}</span>{" "}
+                </h2>
+                <h2>
+                  Website : <span>{job.website}</span>{" "}
+                </h2>
+                <h2>
+                  Salary : <span>{job.salary}</span>{" "}
+                </h2>
+                <h2>
+                  Vacancy : <span>{job.vacancy}</span>{" "}
+                </h2>
+              </div>
 
-        <div className="desc">
-          <h3>Job Description:</h3>
-          <p>{job.description}</p>
-          <h3>Job Requriements:</h3>
-          <p>{job.requirements}</p>
-        </div>
+              <div className="desc">
+                <h3>Job Description:</h3>
+                <p>{job.description}</p>
+                <h3>Job Requriements:</h3>
+                <p>{job.requirements}</p>
+              </div>
+            </div>
+          </>
+        )}
       </div>
-    </div>
+      {applying &&
+        createPortal(
+          <LoaderContainer />,
+          document.getElementById("modal-container")
+        )}
+    </>
   );
 }
 
